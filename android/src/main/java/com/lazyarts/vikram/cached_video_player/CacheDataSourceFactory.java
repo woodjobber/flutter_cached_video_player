a35e3c99a0b2cbce43ff396a5cf92d225badb111
soer <474436731@qq.com>
2024-01-18 13:00:11 +0800

    fix:  1、AAPT: error: resource android:attr/lStar not found. 2、 Upgrade com.google.android.exoplayer:exoplayer-core:2.18.1

--- android/src/main/java/com/lazyarts/vikram/cached_video_player/CacheDataSourceFactory.java
+++ android/src/main/java/com/lazyarts/vikram/cached_video_player/CacheDataSourceFactory.java
@@ -17,31 +17,24 @@ class CacheDataSourceFactory implements DataSource.Factory {
     private final Context context;
     private DefaultDataSource.Factory defaultDatasourceFactory;
     private final long maxFileSize, maxCacheSize;
-
     private DefaultHttpDataSource.Factory defaultHttpDataSourceFactory;
-
     CacheDataSourceFactory(Context context, long maxCacheSize, long maxFileSize) {
         super();
         this.context = context;
         this.maxCacheSize = maxCacheSize;
         this.maxFileSize = maxFileSize;
+        defaultHttpDataSourceFactory = new DefaultHttpDataSource.Factory()
+                .setConnectTimeoutMs(DefaultHttpDataSource.DEFAULT_CONNECT_TIMEOUT_MILLIS)
+                .setReadTimeoutMs(DefaultHttpDataSource.DEFAULT_READ_TIMEOUT_MILLIS);
 
-        defaultHttpDataSourceFactory = new DefaultHttpDataSource.Factory();
-        defaultHttpDataSourceFactory.setUserAgent("ExoPlayer");
-        defaultHttpDataSourceFactory.setAllowCrossProtocolRedirects(true);
     }
-
-    void setHeaders(Map<String, String> httpHeaders) {
+    void setHeaders(Map<String, String> httpHeaders){
         defaultHttpDataSourceFactory.setDefaultRequestProperties(httpHeaders);
     }
-
     @Override
     public DataSource createDataSource() {
         DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter.Builder(context).build();
-
         defaultDatasourceFactory = new DefaultDataSource.Factory(this.context, defaultHttpDataSourceFactory);
-        defaultDatasourceFactory.setTransferListener(bandwidthMeter);
-
         SimpleCache simpleCache = SimpleCacheSingleton.getInstance(context, maxCacheSize).simpleCache;
         return new CacheDataSource(simpleCache, defaultDatasourceFactory.createDataSource(),
                 new FileDataSource(), new CacheDataSink(simpleCache, maxFileSize),
